2f1efda4ee2c7359597823f3be916d0bf89ee0b7
Peter Rifkind <prifkind@gmail.com>
2023-08-07 14:55:42 -0400

    Added conditional rendering of recipes and shopping list based on navigation props

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  showRecipes: boolean = true;
+  showShoppingList: boolean = false;
 
+  @Output() showRecipesEmitter: EventEmitter<boolean> =
+    new EventEmitter<boolean>();
+  @Output() showShoppingListEmitter: EventEmitter<boolean> =
+    new EventEmitter<boolean>();
+
+  handleShowRecipes() {
+    this.showRecipes = !this.showRecipes;
+    this.showRecipesEmitter.emit(this.showRecipes);
+  }
+
+  handleShowShoppingList() {
+    this.showShoppingList = !this.showShoppingList;
+    this.showShoppingListEmitter.emit(this.showShoppingList);
+  }
 }
