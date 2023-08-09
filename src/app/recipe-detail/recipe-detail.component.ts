4a3dd110500c0e5b9850199a31d09b28b16b86a1
Peter Rifkind <prifkind@gmail.com>
2023-08-09 11:59:58 -0400

    Added functionality for inputs in shopping list

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css'],
 })
-export class RecipeDetail {}
+export class RecipeDetail {
+  @Input() recipe: Recipe = { name: '', description: '', imagePath: '' };
+}
--- src/app/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrls: ['./shopping-list-edit.component.css']
+  styleUrls: ['./shopping-list-edit.component.css'],
 })
-export class ShoppingListEditComponent {
+export class ShoppingListEditComponent  {
+  @ViewChild('nameInput', { static: true }) ingredientNameRef!: ElementRef;
+  @ViewChild('amountInput', { static: true }) ingredientAmountRef!: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddIngredients() {
+    const ingredientName = this.ingredientNameRef.nativeElement.value;
+    const ingredientAmount = this.ingredientAmountRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount)
+    this.ingredientAdded.emit(newIngredient)
+  }
 }
